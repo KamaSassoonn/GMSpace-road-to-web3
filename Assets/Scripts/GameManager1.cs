70fa05641732d3a6109bc44c11f5f478c925d125
Yi <laoyipay@msn.com>
2022-02-07 06:17:17 +0800

    Enter and Left loading

--- Assets/Scripts/GameManager1.cs
+++ Assets/Scripts/GameManager1.cs
@@ -33,5 +33,30 @@ public class GameManager1 : MonoBehaviourPunCallbacks
     }
     
     
-   
+     public override void OnPlayerEnteredRoom(Player other)
+    {
+        Debug.LogFormat("OnPlayerEnteredRoom() {0}", other.NickName); // not seen if you're the player connecting
+
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+            
+            LoadArena();
+        }
+    }
+
+
+    public override void OnPlayerLeftRoom(Player other)
+    {
+        Debug.LogFormat("OnPlayerLeftRoom() {0}", other.NickName); // seen when other disconnects
+
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.LogFormat("OnPlayerLeftRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+            
+            LoadArena();
+        }
+    }
 }
