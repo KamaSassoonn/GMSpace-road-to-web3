0b379f175048d302ee3083c07c981f58242ec011
Yi <laoyipay@msn.com>
2022-02-07 06:19:45 +0800

    Enter custom name

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -268,7 +268,22 @@ public class Launcher : MonoBehaviourPunCallbacks
         // SceneManager.LoadScene(0);
     }
     
+     /// <summary>
+    /// Sets the name of the player, and save it in the PlayerPrefs for future sessions.
+    /// </summary>
+    /// <param name="value">The name of the Player</param>
+    public void SetPlayerName(string value)
+    {
+        // #Important
+        if (string.IsNullOrEmpty(value))
+        {
+            Debug.LogError("Player Name is null or empty");
+            return;
+        }
+        PhotonNetwork.NickName = value;
 
+        PlayerPrefs.SetString(playerNamePrefKey, value);
+    }
     
     
     
